e1772522f159765c3481a19ef9ee24a8f54eb59e
Andrea <adestajo@sprout.ph>
2025-08-12 14:43:36 +0800

    fix: Update resume link and improve formatting in skills.js

--- constants/skills.js
+++ constants/skills.js
@@ -1,32 +1,34 @@
-import nodeLogoUrl from "../public/logo/node.png"
-import vueLogoUrl from "../public/logo/vue.png"
-import nuxtLogoUrl from "../public/logo/nuxt.png"
-import reactLogoUrl from "../public/logo/react.png"
+import nodeLogoUrl from "../public/logo/node.png";
+import vueLogoUrl from "../public/logo/vue.png";
+import nuxtLogoUrl from "../public/logo/nuxt.png";
+import reactLogoUrl from "../public/logo/react.png";
 
 export const SKILLS = [
-    {
-        logo: nodeLogoUrl,
-        title: 'Node Js',
-        description: '1 year backend experience with node js.'
-    },
-    {
-        logo: nuxtLogoUrl,
-        title: 'Nuxt Js',
-        description: '3 years experience with Nuxt. Bilidali and Commisari was my main project that was based on this framework.'
-    },
-    {
-        logo: reactLogoUrl,
-        title: 'React Js/Next js',
-        description: 'No experience yet with this javascript library but still learning React Js. Had a basic knowledge with this libraries and this porfolio was based on React.'
-    },
-    {
-        logo: vueLogoUrl,
-        title: 'Vue Js',
-        description: '3 years experience with this javascript framework.'
-    },
-    // {
-    //     logo: '',
-    //     title: '',
-    //     description: ''
-    // }
-]
+  {
+    logo: nodeLogoUrl,
+    title: "Node Js",
+    description: "1 year backend experience with node js.",
+  },
+  {
+    logo: nuxtLogoUrl,
+    title: "Nuxt Js",
+    description:
+      "3 years experience with Nuxt. Bilidali and Commisari was my main project that was based on this framework.",
+  },
+  {
+    logo: reactLogoUrl,
+    title: "React Js/Next js",
+    description:
+      "No experience yet with this javascript library but still learning React Js. Had a basic knowledge with this libraries and this porfolio was based on React.",
+  },
+  {
+    logo: vueLogoUrl,
+    title: "Vue Js",
+    description: "5 years experience with this javascript framework.",
+  },
+  // {
+  //     logo: '',
+  //     title: '',
+  //     description: ''
+  // }
+];
