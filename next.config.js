ce6b52d5da071823387045599af23ef8af3eeb7e
Andrea <adestajo@sprout.ph>
2026-02-25 00:32:16 +0800

    Revert "Merge pull request #6 from Destnyl/2025_AUG_28/Enhancement/andrea"
    This reverts commit cb87698a18341d615148fab9e911acdd925fccf2, reversing
    changes made to e58539ee660a4d252b215db0f1c1ac4973fb8cf4.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: false,
-    formats: ['image/webp', 'image/avif'],
-  },
 }
 
-module.exports = nextConfig
+        module.exports = nextConfig
